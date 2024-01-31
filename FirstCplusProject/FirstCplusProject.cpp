889b6d0d4da345695fcaaefb4547d48ac76a9625
Mathys Hymon <mathys.hymon@gmail.com>
2024-01-31 15:04:39 +0100

    [fix] bank account

--- FirstCplusProject/FirstCplusProject.cpp
+++ FirstCplusProject/FirstCplusProject.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 using namespace std;
 
+void EarMoney(int wallet, int wage) {
+    wallet += wage;
+    wallet -= (int)(wage / 4);
+}
+
+
+
 int main()
 {
     cout << "What is your name ?" << endl;
@@ -9,7 +16,15 @@ int main()
     cout << "Hello " << answer << endl << ", how are you today ?" << endl;
 
 
-    int x = 14;
-    cout << "The value " << x << " is stored in " << &x << endl;
+    int money = 14;
+    int* bankAccount = &money;
+
+    cout << "The value " << money << " is stored in " << &money << endl;
     cout << "The value " << answer << " is stored in " << &answer << endl;
+    int earnmoney;
+    cout << "how many do you earn ?" << endl;
+    cin >> earnmoney;
+    EarMoney(money,earnmoney);
+
+    cout << "okay, I added " << earnmoney << " to your wallet, now you have " << money << endl;
 }
